2c90b11772d57dc299d640d8532cd434cf8ab344
Davanon <davanon2014@gmail.com>
2018-02-12 17:43:45 +0530

    First

---
+++ src/main/java/com/iqmsoft/jms/ReceiveMessageUI.java
@@ -0,0 +1,40 @@
+package com.iqmsoft.jms;
+
+import com.iqmsoft.jms.messaging.Broadcaster;
+import com.vaadin.annotations.Push;
+import com.vaadin.annotations.Theme;
+import com.vaadin.cdi.CDIUI;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.ContentMode;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+import java.util.function.Consumer;
+
+@Push
+@CDIUI("receive-message")
+@Theme("serverpushexampletheme")
+public class ReceiveMessageUI extends UI implements Consumer<String> {
+
+    private final VerticalLayout messages = new VerticalLayout();
+
+    @Override
+    protected void init(final VaadinRequest vaadinRequest) {
+        messages.addComponent(new Label("<h2>Received Messages</h2>", ContentMode.HTML));
+        setContent(messages);
+        Broadcaster.register(this);
+    }
+
+    @Override
+    public void detach() {
+        Broadcaster.unregister(this);
+        super.detach();
+    }
+
+    @Override
+    public void accept(final String message) {
+        access(() -> messages.addComponent(new Label(message)));
+    }
+
+}
---
+++ src/main/java/com/iqmsoft/jms/SendMessageUI.java
@@ -0,0 +1,43 @@
+package com.iqmsoft.jms;
+
+import com.iqmsoft.jms.messaging.MessageSenderBean;
+import com.vaadin.annotations.Theme;
+import com.vaadin.cdi.CDIUI;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.ContentMode;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+import javax.ejb.EJB;
+
+@CDIUI("send-message")
+@Theme("serverpushexampletheme")
+public class SendMessageUI extends UI {
+
+    @EJB
+    private MessageSenderBean sender;
+
+    @Override
+    protected void init(final VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.addComponent(new Label("<h2>Sending Messages</h2>", ContentMode.HTML));
+
+        final TextField messageField = new TextField();
+        messageField.setCaption("Type the message to send here:");
+
+        Button button = new Button("Send message");
+        button.addClickListener(e -> {
+            final String message = messageField.getValue();
+            sender.send(message);
+            layout.addComponent(new Label("Sent message '" + message + "'"));
+        });
+
+        layout.addComponents(messageField, button);
+
+        setContent(layout);
+    }
+
+}
